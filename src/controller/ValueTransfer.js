d5d3781c5b494dac6521ca63a6e4d0f6d9aa31d2
weslx <wesleyaleixo0950@gmail.com>
2023-12-18 16:48:43 -0300

    Feat: Transfer value + error handling

--- src/controller/ValueTransfer.js
+++ src/controller/ValueTransfer.js
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-class TrasnferValue {
+class TransferValue {
   async send(req, res) {
     const { CpfTransfer, CpfReceber, valor } = req.body;
+
+    if (CpfTransfer === CpfReceber) {
+      return res
+        .status(400)
+        .json("Não é possível transferir dinheiro para o mesmo CPF");
+    }
 
     const UserTransferExiste = await prisma.users.findUnique({
       where: {
@@ -35,8 +40,31 @@ class TrasnferValue {
         .json("Usuario que voce deseja enviar o dinheiro nao existe");
     }
 
-    console.log(UserTransferExiste);
+    if (UserTransferExiste.infousuario[0].saldo < valor) {
+      return res
+        .status(400)
+        .json("Saldo insuficiente para realizar a transferência");
+    }
+
+    await prisma.infousuario.update({
+      where: {
+        id: UserTransferExiste.infousuario[0].id,
+      },
+      data: {
+        saldo: UserTransferExiste.infousuario[0].saldo - valor,
+      },
+    });
+
+    await prisma.infousuario.update({
+      where: {
+        id: UserReceberExiste.infousuario[0].id,
+      },
+      data: {
+        saldo: UserReceberExiste.infousuario[0].saldo + valor,
+      },
+    });
+    return res.status(200).json("Transferência realizada com sucesso");
   }
 }
 
-export default new TrasnferValue();
+export default new TransferValue();
