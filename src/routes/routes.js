8ef5d7aa0b0d02c3c3dcf0a99c3e567c6ff0f2ba
weslx <wesleyaleixo0950@gmail.com>
2023-12-19 12:06:32 -0300

    Feat: Login feature

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import VerificarToken from "../utils/VerificarTokens.js";
 import CreateAccount from "../controller/CreateAccount.js";
+import LoginAccount from "../controller/LoginAccount.js";
 import InfoUsers from "../controller/Deposits.js";
 import UpdateUserStatus from "../controller/UpdateUserStatus.js";
 import ValueTransfer from "../controller/ValueTransfer.js";
@@ -8,6 +9,7 @@ import ValueTransfer from "../controller/ValueTransfer.js";
 const routes = new Router();
 
 routes.post("/criarusuario", CreateAccount.store);
+routes.post("/login", LoginAccount.login);
 routes.post("/depositar", VerificarToken, InfoUsers.store);
 routes.post("/mudar-status", VerificarToken, UpdateUserStatus.update);
 routes.post("/transferencia", VerificarToken, ValueTransfer.send);
