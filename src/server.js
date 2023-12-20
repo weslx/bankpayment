09393099b1bac9d7f000b707de92a6fa144cbd65
weslx <wesleyaleixo0950@gmail.com>
2023-12-20 20:57:35 -0300

    feat: telegram bot routes + webhook

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-import express from "express";
+import BotTelegram from "./utils/TelegramBot.js";
 import app from "./app.js";
 
 const port = 3000;
+const bot = new BotTelegram();
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Running on http://localhost:${port}`);
+  await bot.Webhook();
 });
